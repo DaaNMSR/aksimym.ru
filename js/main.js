ed0f149d9e274cef57359981fb28356d84cca966
DaaNMSR <denispresent4@gmail.com>
2023-10-10 22:56:57 +0300

    add modal window, under revision

--- js/main.js
+++ js/main.js
@@ -3,3 +3,26 @@ document.querySelector('.burger').addEventListener('click', function() {
     document.querySelector('.container').classList.toggle('open');
     document.querySelector('.container-bottom').classList.toggle('open');
 })
+
+let modal = document.getElementById("myModal");
+let btn = document.getElementById("categories-button");
+let openBtn = document.getElementsByClassName("modal_btn")[0];
+let span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+    modal.style.display = "flex";
+}
+
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+openBtn.onclick = function() {
+    modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
